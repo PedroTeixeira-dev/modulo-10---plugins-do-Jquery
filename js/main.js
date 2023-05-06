0b3661bc2eed8f583111c7694d18ae700493bf14
Pedro Teixeira <pedro15at@gmail.com>
2023-05-05 21:35:51 -0300

    "adicionando as mascaras e validacoes"

---
+++ js/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+    $('#carrossel-imagens').slick({
+        autoplay: true,
+    })
+
+    $('.menu-hamburguer').click(function(){
+        $('nav').slideToggle()
+    })
+
+    $('#telefone').mask('(00) 00000-0000',{placeholder:'(__)_____-____'})
+
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            mensagem: {
+                required: true
+            },
+            veiculoDeInteresse: {
+                required: false
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira seu nome',
+            email: 'Por favor, insira seu e-mail',
+            telefone: 'Por favor, insira seu numero'
+        }, 
+        submitHandler: function() {
+            console.log(form)
+        }, 
+        invalidHandler: function(evento, validador) {
+            let campoIncorretos = validador.numberOfInvalids()
+            console.log(campoIncorretos)
+        }
+    })
+
+    $('.lista-veiculos button').click(function(){
+        const destino = $('#contato')
+
+        const nomeVeiculo = $(this).parent().find('h3').text()
+
+        $('#veiculo-interesse').val(nomeVeiculo)
+
+        $('html').animate({
+            scrollTop: destino.offset().top
+        }, 1000)
+    })
+
+})
